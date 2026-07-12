b555d390d087895efbd7932065f472a26c5e6830
Alexander Zhurba <a.zhurba0810@gmail.com>
2026-07-12 12:34:05 +0300

    fix socket

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,7 @@ import { Message } from "../models/message.model.js";
 export const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: "https://audio-player-9gwn.onrender.com",
             credentials: true,
         },
     });
